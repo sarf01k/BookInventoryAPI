e6b0bf2bf5a7bf791687102ab93dc7403a12ba41
sarf01k <isaacksarfo1@gmail.com>
2025-01-25 15:50:04 +0000

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+namespace BookInventoryAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Genre { get; set; } = string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+using BookInventoryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var books = new List<Book> { };
+
+app.MapGet("/books", () =>
+{
+    return books;
+});
+
+app.MapGet("/books/{id}", ([FromRoute] int id) =>
+{
+    var book = books.Find(b => b.Id == id);
+
+    if (book is null)
+        return Results.NotFound("Book does not exist");
+
+    return Results.Ok(book);
+});
+
+app.MapPost("/books", ([FromBody] Book book) =>
+{
+    var newBook = new Book
+    {
+        Title = book.Title,
+        Author = book.Author,
+        Price = book.Price,
+        Genre = book.Genre
+    };
+
+    books.Add(newBook);
+    newBook.Id = books.IndexOf(newBook) + 1;
+
+    return Results.Ok(newBook);
+});
+
+app.MapPut("books/{id}", ([FromRoute] int id, [FromBody] Book bookUpdate) =>
+{
+    var book = books.Find(b => b.Id == id);
+
+    if (book is null)
+        return Results.NotFound("Book does not exist");
+
+    book.Title = bookUpdate.Title;
+    book.Author = bookUpdate.Author;
+    book.Price = bookUpdate.Price;
+    book.Genre = bookUpdate.Genre;
+
+    return Results.Ok(book);
+});
+
+app.MapDelete("books/{id}", ([FromRoute] int id) =>
+{
+    var book = books.Find(b => b.Id == id);
+
+    books.Remove(book);
+
+    return Results.Ok();
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapScalarApiReference();
+}
+
+app.Run();
